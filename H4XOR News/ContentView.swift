ef45972fef21cf20f11d169d30c0091ae0e09348
Bijon Krishna Bairagi <bijon.bairagi@gmail.com>
2025-01-22 15:07:30 +0600

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Bijon Krishna Bairagi on 1/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
