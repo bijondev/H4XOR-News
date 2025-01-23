7fa1ba31203a23af306b55ffb13161a7a71b906c
Bijon Krishna Bairagi <bijon.bairagi@gmail.com>
2025-01-23 11:20:43 +0600

    details page done

---
+++ H4XOR News/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  H4XOR News
+//
+//  Created by Bijon Krishna Bairagi on 1/23/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ H4XOR News/Views/WebView.swift
@@ -0,0 +1,8 @@
+//
+//  WebView.swift
+//  H4XOR News
+//
+//  Created by Bijon Krishna Bairagi on 1/23/25.
+//
+
+import Foundation
